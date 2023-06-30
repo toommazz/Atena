f8acdf398b14ce8a3e7bd09d86b9caf00dde97f9
Rogério Tomazini <rogerio.tomazini@gmail.com>
2023-06-29 22:31:48 -0300

    Change: update command

--- Atena/Atena.Domain.Core/Command/Command.cs
+++ Atena/Atena.Domain.Core/Command/Command.cs
@@ -6,27 +6,27 @@ namespace Atena.Domain.Core
 {
     public abstract class Command : CommandMessage
     {
-        public Command(Guid? id, string situacao = "1", DateTime dataInclusao = default, DateTime dataAlteracao = default)
+        public Command(Guid? id, string status = "1", DateTime included = default, DateTime updated = default)
         {
             Id = id;
-            DataInclusao = dataInclusao;
-            DataAlteracao = dataAlteracao;
-            Situacao = situacao;
+            Included = included;
+            Updated = updated;
+            Status = status;
         }
 
         public Guid? Id { get; protected set; }
 
-        public DateTime DataInclusao { get; protected set; }
+        public DateTime Included { get; protected set; }
 
-        public DateTime DataAlteracao { get; protected set; }
+        public DateTime Updated { get; protected set; }
 
-        public string Situacao { get; protected set; }
+        public string Status { get; protected set; }
 
         public ValidationResult ValidationResult { get; set; } = new ValidationResult();
 
         protected Command()
         {
-            DataInclusao = DateTime.Now.ToBrasilianTimezone();
+             Included = DateTime.Now.ToBrasilianTimezone();
         }
 
         public abstract bool IsValid();
