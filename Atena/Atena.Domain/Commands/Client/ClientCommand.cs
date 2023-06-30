caf50d787ace444507511cca30eefddcf841b794
sergio.batista <sergiomelo1405@gmail.com>
2023-06-29 22:41:51 -0300

    change: created ClientCommand

--- Atena/Atena.Domain/Commands/Client/ClientCommand.cs
+++ Atena/Atena.Domain/Commands/Client/ClientCommand.cs
@@ -1,8 +1,14 @@
-﻿
+﻿using Atena.Domain.Core;
 
 namespace Atena.Domain.Commands.Client
 {
-    public  class ClientCommand //: Command
+    public  class ClientCommand : Command
     {
+        public ClientCommand(Guid? id) : base (id) { }
+        
+        public override bool IsValid()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
