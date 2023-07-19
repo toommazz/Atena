c3657d150010a86050f316a3ed0826a77fe5cf5e
sergio.batista <sergiomelo1405@gmail.com>
2023-07-18 21:32:43 -0300

    changes: update

--- Atena/Atena.Domain/CommandHandlers/Contact/Phone/RemovePhoneCommandHandler.cs
+++ Atena/Atena.Domain/CommandHandlers/Contact/Phone/RemovePhoneCommandHandler.cs
@@ -10,7 +10,7 @@ namespace Atena.Domain.CommandHandlers
 {
     public class RemovePhoneCommandHandler : CommandHandlerRemove<RemovePhoneCommand, Phone, RemovedPhoneEvent>
     {
-        public RemovePhoneCommandHandler(IMediatorHandler mediator, IMapper mapper, IRepository<PhoneType> repository) : base(mediator, mapper, repository)
+        public RemovePhoneCommandHandler(IMediatorHandler mediator, IMapper mapper, IRepository<Phone> repository) : base(mediator, mapper, repository)
         {
         }
     }
