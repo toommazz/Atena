c6590c5cbd9914911f85c279e91c830c31279b0a
Rogério Tomazini <rogerio.tomazini@gmail.com>
2023-07-18 22:45:35 -0300

    Change: Updatede IAdressRepository

--- Atena/Atena.Domain/Repositories/IAddressRepository.cs
+++ Atena/Atena.Domain/Repositories/IAddressRepository.cs
@@ -3,6 +3,5 @@ using Atena.Domain.Model;
 
 namespace Atena.Domain.Repositories
 {
-<<<<<<<< HEAD:Atena/Atena.Domain/Repositories/IAddressRepository.cs
     public interface IAddressRepository : IRepository<Address> { }
 }
