69aaff020212265e80e6de788b2352844f1b95ff
Rogério Tomazini <rogerio.tomazini@gmail.com>
2023-07-24 22:26:06 -0300

    Change: Included script PersonType.sql

---
+++ Atena/Atena.Infra/Script/PersonType.sql
@@ -0,0 +1,15 @@
+﻿-- Atena.dbo.PersonType definition
+
+-- Drop table
+
+-- DROP TABLE Atena.dbo.PersonType;
+
+CREATE TABLE Atena.dbo.PersonType (
+	Id uniqueidentifier NOT NULL,
+	Name varchar(100) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+	Description varchar(150) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+	Included datetime NOT NULL,
+	Updated datetime NULL,
+	Status varchar(2) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+	CONSTRAINT PersonType_PK PRIMARY KEY (Id)
+);
